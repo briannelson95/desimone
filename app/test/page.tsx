da7e63b42be34e1738035c65be07d2204171413f
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-02-26 12:43:04 -0500

    autoplay not working right now but looking good

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 export default function page() {
     return (
         <div className='overflow-x-hidden'>
-            <div className='bg-black/50 h-screen flex flex-col justify-center items-center text-white text-center relative'>
+            <div className='bg-black/50 h-screen flex flex-col justify-center items-center text-white text-center'>
                 <VideoBackground videoId='pCf4QvfOJ34' />
-                {/* <div className='absolute h-screen flex flex-col justify-center items-center text-white text-center'>
+                <div className='absolute h-screen flex flex-col justify-center items-center text-white text-center'>
                     <h1 className='text-5xl font-extrabold'>Nicholas Desimone</h1>
                     <p className='text-xl'>This is a test of the fonts</p>
-                </div> */}
+                </div>
             </div>
-            <div>
-                <h2 className='text-3xl font-bold'>This is another section</h2>
-                <p className='text-xl'>More text will go here</p>
+            <div className='m-4'>
+                <h2 className='text-3xl font-bold'>Another Section</h2>
+                <p className='text-xl'>More data will go here</p>
             </div>
         </div>
     )
