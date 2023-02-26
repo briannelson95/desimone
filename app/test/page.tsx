d3cabe711f21e80b889ff29dda4000fa9b39b8db
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-02-26 12:31:14 -0500

    trying to get autoplay to work

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 export default function page() {
     return (
-        <div className=''>
+        <div className='overflow-x-hidden'>
             <div className='bg-black/50 h-screen flex flex-col justify-center items-center text-white text-center relative'>
                 <VideoBackground videoId='pCf4QvfOJ34' />
-                <div className='absolute h-screen flex flex-col justify-center items-center text-white text-center'>
+                {/* <div className='absolute h-screen flex flex-col justify-center items-center text-white text-center'>
                     <h1 className='text-5xl font-extrabold'>Nicholas Desimone</h1>
                     <p className='text-xl'>This is a test of the fonts</p>
-                </div>
+                </div> */}
             </div>
             <div>
-                <h2 className='text-3xl font-bold dark:text-white'>This is another section</h2>
+                <h2 className='text-3xl font-bold'>This is another section</h2>
                 <p className='text-xl'>More text will go here</p>
             </div>
         </div>
--- components/VideoBackground.tsx
+++ components/VideoBackground.tsx
@@ -6,18 +6,19 @@ type Props = {
 
 const VideoBackground = ({ videoId }: Props) => {
     return (
-        <div className="relative -top-0 left-0 h-[325%] w-[325%] lg:-left-36 lg:w-[120%] lg:h-[120%] z-[-1]">
+        <div className="relative top-0 left-0 h-[325%] w-[325%] lg:-left-36 lg:w-[120%] lg:h-[120%] z-[-1]">
             <iframe
-                src={`https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}&controls=0&modestbranding=1`}
+                src={`https://www.youtube.com/embed/${videoId}?controls=0&modestbranding=1`}
                 allow="autoplay; encrypted-media"
                 allowFullScreen
-                style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                }}
+                className="absolute top-0 left-0 w-full h-full"
+                // style={{
+                //     position: "absolute",
+                //     top: 0,
+                //     left: 0,
+                //     width: "100%",
+                //     height: "100%",
+                // }}
             />
         </div>
     );
