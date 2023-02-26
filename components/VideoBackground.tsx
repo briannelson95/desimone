5cb42116a571df348c6e136e17936078c1ca6f7e
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-02-26 13:13:30 -0500

    Update VideoBackground.tsx

--- components/VideoBackground.tsx
+++ components/VideoBackground.tsx
@@ -8,7 +8,7 @@ const VideoBackground = ({ videoId }: Props) => {
     return (
         <div className="relative top-0 left-0 h-full w-full z-[-1]">
             <iframe
-                src={`https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}&controls=0&modestbranding=1`}
+                src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&modestbranding=1`}
                 allow="autoplay; encrypted-media"
                 allowFullScreen
                 className="absolute top-0 left-0 w-full h-full"
