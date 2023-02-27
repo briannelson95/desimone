c7f1c9d5faf4ef45fc3444383b4391d06a8d682b
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-02-27 15:22:54 -0500

    mobile autoplay

--- components/VideoBackground.tsx
+++ components/VideoBackground.tsx
@@ -6,10 +6,23 @@ type Props = {
 
 const VideoBackground = ({ videoId }: Props) => {
     return (
-        <div className="relative top-0 left-0 h-full w-full z-[-1] overflow-y-hidden">
-            <video className="absolute -top-20 left-0 h-[125%] w-[125%]" autoPlay loop muted>
+        <div 
+            className="relative top-0 left-0 h-full w-full z-[-1] overflow-y-hidden"
+            dangerouslySetInnerHTML={{ __html: `
+                <video class="absolute -top-20 left-0 h-[125%] w-[125%]" loop muted autoplay playsinline>
+                    <source src="./background-video.mp4" type="video/mp4" />
+                </video
+            `}}
+        >
+            {/* <video 
+                className="absolute -top-20 left-0 h-[125%] w-[125%]" 
+                dangerouslySetInnerHTML={{ __html: 
+                    `loop muted autoplay playsinline
+                    <source src="./background-video.mp4" type="video/mp4" />`
+                }}
+            >
                 <source src="./background-video.mp4" type="video/mp4" />
-            </video>
+            </video> */}
             {/* <iframe
                 src={`https://www.youtube.com/embed/${videoId}?autoplay=1&playlist=${videoId}&loop=1&controls=0&modestbranding=1`}
                 allow="autoplay; encrypted-media"
