dcb9ad2728205c6a0b88541c7de6d4c9f4e91953
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-04-21 12:45:01 -0400

    Create page.tsx

---
+++ app/(public)/[slug]/page.tsx
@@ -0,0 +1,39 @@
+import { RichTextComponent } from "@/components/RichTextComponent";
+import { allPages } from "@/lib/queries";
+import { client } from "@/lib/sanity.client";
+import { PortableText } from "@portabletext/react";
+import { groq } from "next-sanity"
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+    const query = groq`*[_type == "pages" && !(slug.current match "portfolio") && !(slug.current match "contact")]{ slug }`;
+
+    const slugs: any[] = await client.fetch(query);
+    const slugRoutes = slugs.map((slug) => slug.slug.current);
+
+    return slugRoutes.map((slug) => ({
+        slug
+    }))
+}
+
+export default async function Page({params: {slug}}: PageProps) {
+    const page: any = await client.fetch(allPages, { slug });
+
+    if (!page) {
+        notFound()
+    }
+
+    console.log(page)
+
+    return (
+        <main className="px-4 md:px-8 lg:px-32">
+            <h1>{page.title}</h1>
+            <section>
+                {page.body ?
+                    <PortableText value={page.body} components={RichTextComponent} />
+                    : null
+                }
+            </section>
+        </main>
+    )
+}
